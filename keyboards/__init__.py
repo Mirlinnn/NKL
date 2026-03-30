bec77341fab1812dc2bfb258417bb089dd4c86e7
Mirlinnn <gayday10@inbox.ru>
2026-03-31 00:47:04 +0900

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,18 @@
-.
+from .main import get_main_keyboard, get_back_keyboard
+from .platforms import get_platform_keyboard
+from .telegram import get_telegram_menu
+from .vk import get_vk_menu
+from .instagram import get_instagram_menu
+from .tiktok import get_tiktok_menu
+from .stars import get_stars_menu
+
+__all__ = [
+    'get_main_keyboard',
+    'get_back_keyboard',
+    'get_platform_keyboard',
+    'get_telegram_menu',
+    'get_vk_menu',
+    'get_instagram_menu',
+    'get_tiktok_menu',
+    'get_stars_menu',
+]
