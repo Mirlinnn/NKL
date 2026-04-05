0c67d98309a5c4b7b813027f2cb653ca40c29d5f
Mirlinnn <gayday10@inbox.ru>
2026-04-05 13:00:23 +0900

    Update payment.py

--- handlers/payment.py
+++ handlers/payment.py
@@ -75,7 +75,7 @@ async def create_heleket_payment(amount: float, order_id: str, description: str,
     payload = {
         "amount": f"{amount:.2f}",
         "currency": "RUB",           # фиатная валюта счёта
-        "to_currency": "USDT",       # криптовалюта для оплаты
+        "to_currency": "RUB",       # криптовалюта для оплаты
         "order_id": order_id,
         # "course_source": "Binance", # опционально: источник курса
     }
